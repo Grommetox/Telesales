7b71ddddbd117b8ef8e1277ccac0a843980e6674
Rashad Asgarbayli <rashad2985@gmail.com>
2014-12-09 17:56:32 +0100

    Eclipse se

---
+++ Java/Telesales-GUI/src/telesales/gui/TelesalesGUI.java
@@ -0,0 +1,21 @@
+/**
+ * This class is the main class that will run the GUI.
+ * The class will only start the GUI-Tool.
+ */
+package telesales.gui;
+
+/**
+ * @author rasgarbayli
+ * @version 1.0
+ */
+public class TelesalesGUI {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
