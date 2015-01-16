3a1d4c65b4ea516a8ea8cd4286caea5002b60e50
Rashad Asgarbayli <rashad2985@gmail.com>
2015-01-16 18:06:47 +0100

    BorderPane added to the GUI as a root element

--- 3 Implementation/TelesalesGUI/src/application/Main.java
+++ 3 Implementation/TelesalesGUI/src/application/Main.java
@@ -1,16 +1,23 @@
 package application;
 	
+import gui.elements.BorderPaneRoot;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.HBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			
+			HBox testElement = new HBox();
+			testElement.getChildren().add(new Button("Hello world"));
+			
+			BorderPaneRoot root = new BorderPaneRoot(new TabPane(), testElement);
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
