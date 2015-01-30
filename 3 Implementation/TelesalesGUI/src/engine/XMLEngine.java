e2d047f42f3c20677ed1a86ae040d24b270d5450
Rashad Asgarbayli <rashad2985@gmail.com>
2015-01-30 17:21:57 +0100

    TabPaneRoot implemented

---
+++ 3 Implementation/TelesalesGUI/src/engine/XMLEngine.java
@@ -0,0 +1,17 @@
+package engine;
+
+/**
+ * This class implements engine to write/read into/from XML-Files.
+ * @author rasgarbayli
+ * @version 1.0
+ */
+public class XMLEngine {
+
+	/**
+	 * 
+	 */
+	public XMLEngine() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
